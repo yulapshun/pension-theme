b6c00690a9e23cec325a5f0ab644e89888b85e36
yulapshun <yulapshun@gmail.com>
2016-05-07 15:59:40 +0800

    Fix grid style

--- template-parts/content.php
+++ template-parts/content.php
@@ -7,51 +7,38 @@
  * @since Twenty Sixteen 1.0
  */
 ?>
+<article style="background-image: url(<?php echo the_post_thumbnail_url(); ?>)" id="post-<?php the_ID(); ?>" <?php post_class('post-grid'); ?>>
+  <a href="<?php echo esc_url( get_permalink() ); ?>">
+    <div class="article-mask">
+      <header class="entry-header">
+	<?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
+	  <span class="sticky-post"><?php _e( 'Featured', 'twentysixteen' ); ?></span>
+	<?php endif; ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class('post-grid'); ?>>
-	<header class="entry-header">
-		<?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
-			<span class="sticky-post"><?php _e( 'Featured', 'twentysixteen' ); ?></span>
-		<?php endif; ?>
+	<?php the_title( sprintf( '<div class="entry-title">'), '</div>' ); ?>
+      </header><!-- .entry-header -->
+      <div class="entry-content">
+	<?php
+	/* translators: %s: Name of current post */
+	the_excerpt();
+	/* the_content( sprintf(
+	__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
+	get_the_title()
+	) ); */
 
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-	</header><!-- .entry-header -->
+	wp_link_pages( array(
+	  'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentysixteen' ) . '</span>',
+	  'after'       => '</div>',
+	  'link_before' => '<span>',
+	  'link_after'  => '</span>',
+	  'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>%',
+	  'separator'   => '<span class="screen-reader-text">, </span>',
+	) );
+	?>
+      </div><!-- .entry-content -->
+      <footer class="entry-footer">
 
-	<?php twentysixteen_excerpt(); ?>
-
-	<?php twentysixteen_post_thumbnail(); ?>
-
-	<div class="entry-content">
-		<?php
-			/* translators: %s: Name of current post */
-			the_content( sprintf(
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
-				get_the_title()
-			) );
-
-			wp_link_pages( array(
-				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentysixteen' ) . '</span>',
-				'after'       => '</div>',
-				'link_before' => '<span>',
-				'link_after'  => '</span>',
-				'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>%',
-				'separator'   => '<span class="screen-reader-text">, </span>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php twentysixteen_entry_meta(); ?>
-		<?php
-			edit_post_link(
-				sprintf(
-					/* translators: %s: Name of current post */
-					__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
-					get_the_title()
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-		?>
-	</footer><!-- .entry-footer -->
+      </footer><!-- .entry-footer -->
+    </div><!-- .article-mask -->
+  </a>
 </article><!-- #post-## -->
